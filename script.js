f95035b3519c0ef109e30e319623436909eda864
Precious <realoeprecious@gmail.com>
2026-09-03 09:06:47 -0700

    Completed my title logic.

--- script.js
+++ script.js
@@ -1,48 +1,61 @@
 // Delete function
 
-const deleteBtn = document.getElementById("deleteBtn")
+const deleteBtn = document.getElementById("deleteBtn");
 
-const topics_comments = document.getElementById("topics_comments")
-
+const topics_comments = document.getElementById("topics_comments");
 
 deleteBtn.addEventListener("click", () => {
-   topics_comments.classList.toggle("hideText")
-})
-
+  topics_comments.classList.toggle("hideText");
+});
 
 // Add comment
 
-const commentForm = document.getElementById("commentForm")
-const commentInput = document.getElementById("commentInput")
-const userCommentList = document.getElementById("userCommentList")
+const commentForm = document.getElementById("commentForm");
+const commentInput = document.getElementById("commentInput");
+const userCommentList = document.getElementById("userCommentList");
 
-const commentText = document.getElementById("commentText")
-
+const commentText = document.getElementById("commentText");
 
 commentForm.addEventListener("submit", (event) => {
-     event.preventDefault()
+  event.preventDefault();
 
-    const userCommentInput = commentInput.value.trim() 
-    console.log(userCommentInput)
+  const userCommentInput = commentInput.value.trim();
+  console.log(userCommentInput);
 
-    if(userCommentInput === "") {
-        alert("Leave a comment!")
-        return
-    }else{
-    const li = document.createElement("li")
-    li.textContent = userCommentInput
-    userCommentList.append(li)
+  if (userCommentInput === "") {
+    alert("Leave a comment!");
+    return;
+  } else {
+    const li = document.createElement("li");
+    li.textContent = userCommentInput;
+    userCommentList.append(li);
 
     // Update count
-    const count = userCommentList.children.length
-    commentText.textContent = `Comments (${count})`
-    
-    commentInput.value = ""
-    }
+    const count = userCommentList.children.length;
+    commentText.textContent = `Comments (${count})`;
+
+    commentInput.value = "";
+  }
+});
+
+// Update Talks.
+
+const userTopic = document.getElementById("userTopic");
+
+const talk_summaryForm = document.getElementById("talk_summaryForm");
+
+const titleInput = document.getElementById("titleInput");
 
 
-})
 
+talk_summaryForm.addEventListener("submit", (event) => {
+  event.preventDefault();
 
+  const talkInput = titleInput.value;
+
+  userTopic.textContent = talkInput;
+
+ 
+});
 
 
